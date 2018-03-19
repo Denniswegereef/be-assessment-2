1b213ed7d707250ac4a3b7d2e58edc4324a653fc
Dennis Wegereef <denniswegeeef@gmail.com>
2018-03-19 14:06:09 +0100

    Added a gulp file for the sass management

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
-var gulp = require('gulp')
+'use strict'
 
-gulp.task('test', function() {
-    console.log('hello')
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+
+    connect = require('gulp-connect'),
+
+    gutil = require('gulp-util'),
+    chalk = require('chalk')
+
+
+const dirs = {
+    src: './src',
+    dest: './static'
+}
+const paths = {
+    css: {
+        main: `${dirs.src}/scss/main.scss`,
+        watch: `${dirs.src}/scss/**/*.scss`,
+        dest: `${dirs.dest}`
+    }
+}
+
+gulp.task('sass', () => {
+    gulp.src(paths.css.main)
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(connect.reload())
 })
+
+gulp.task('watch', () => {
+    gulp.watch(paths.css.watch, ['sass'])
+
+    gutil.log(chalk.yellow('Watch is running'))
+})
+
+gulp.task('default', ['sass', 'watch'])
